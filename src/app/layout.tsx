a5b81f1b40b9f4d6f7e6c7d51eeae620a3c6c29f
Gustavo Vasconcelos <gvf@Gustavos-MacBook-Air.local>
2025-03-15 22:58:15 +1000

    solve GTM issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,15 @@
 import type { Metadata } from "next"
+
 import { Roboto } from "next/font/google"
-import "./globals.css"
-import { Toaster } from "@/components/ui/toaster"
 import Script from "next/script"
-import { GoogleTagManager } from '@next/third-parties/google'
+
+import { Toaster } from "@/components/ui/toaster"
+import "./globals.css"
+
 
 export const metadata: Metadata = {
   title: "FlyTop Travels",
-  description: "FlyTop description",
+  description: "FlyTop Travels has the best prices ever",
 }
 
 const roboto = Roboto({
@@ -18,13 +20,29 @@ const roboto = Roboto({
 })
 
 export default function RootLayout({children}: { children: React.ReactNode }) {
+  const gtmId = process.env.NEXT_PUBLIC_GTM_ID
+
   return (
     <html lang="en-US" className={`${roboto.variable}`}>
-      <GoogleTagManager gtmId="GTM-5Z3KSBV5" />
+      <head>
+        {/* Script do GTM na tag <head> */}
+        <Script 
+          id="gtm-snippet"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${gtmId}')
+            `
+          }}
+        />
+      </head>
       <body className="text-sky-900 flex flex-col min-h-screen">
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-5Z3KSBV5"
+            src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`}
             height="0"
             width="0"
             style={{ display: 'none', visibility: 'hidden' }}
